bb9aeff42016e5850bb5539b79af580489725c6a
Christopher Meza <m4tchb0x87@gmail.com>
2020-11-04 19:37:00 -0600

    Added hover effect and tweaked padding.

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// Hover over "mepick" swap.
+document.getElementById("mepick").onmouseover = function () {
+  mepickOver();
+};
+
+function mepickOver() {
+  document.getElementById("mepick").src = "../images/mepick-2.png";
+}
+
+// Hover out "mepick" swap.
+document.getElementById("mepick").onmouseout = function () {
+  mepickOut();
+};
+
+function mepickOut() {
+  document.getElementById("mepick").src = "../images/mepick-1.png";
+}
