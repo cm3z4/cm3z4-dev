1f4711f89d92502736e7e6fe7cef5e8ac99b2f31
cm3z4 <m4tchb0x87@gmail.com>
2021-01-31 22:18:19 -0600

    Monochrome January post.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
-// HOME CODE
+// HOME [ START ]
+
 if (document.getElementById("home")) {
   // Hover over "me" color swap.
   document.getElementById("me").onmouseover = function () {
@@ -6,7 +7,7 @@ if (document.getElementById("home")) {
   };
 
   function meColor() {
-    document.getElementById("me").src = "images/arr/me-color.png";
+    document.getElementById("me").src = "/images/arr/me-color.png";
   }
 
   // Hover out "me" monochrome swap.
@@ -15,15 +16,19 @@ if (document.getElementById("home")) {
   };
 
   function meMono() {
-    document.getElementById("me").src = "images/arr/me-mono.png";
+    document.getElementById("me").src = "/images/arr/me-mono.png";
   }
 }
 
-// POST CODE
+// HOME [ END ]
+
+// -----------------------------------------------------------------------
+
+// BLOG [ START ]
 
 // Append all the published blog posts.
-function appendPosts(data) {
-  console.log(data.posts);
+function appendBlogPosts(data) {
+  // console.log(data.posts);
   for (let i = 0; i < data.posts.length; i++) {
     let postDate = data.posts[i].postDate;
     let postName = data.posts[i].postName;
@@ -38,11 +43,51 @@ function appendPosts(data) {
     nodeA.innerText = postName;
     nodeA.classList.add("post-links");
     node.appendChild(nodeA);
-    document.getElementById("posts").appendChild(node);
+    document.getElementById("blog-posts").appendChild(node);
   }
 }
 
 // Fetch all the published blog posts.
-fetch("../posts/posts.json")
-  .then((response) => response.json())
-  .then((data) => appendPosts(data));
+let checkBlog = document.getElementById("blog-posts");
+if (checkBlog !== null) {
+  fetch("/posts/blog/blog.json")
+    .then((response) => response.json())
+    .then((data) => appendBlogPosts(data));
+}
+
+// BLOG [ END ]
+
+// -----------------------------------------------------------------------
+
+// MONOCHROME [ START ]
+
+// Append all the published monochrome posts.
+function appendMonochromePosts(data) {
+  // console.log(data.posts);
+  for (let i = 0; i < data.posts.length; i++) {
+    let postDate = data.posts[i].postDate;
+    let postName = data.posts[i].postName;
+    let postPath = data.posts[i].postPath;
+    let node = document.createElement("li");
+    let nodeSpan = document.createElement("span");
+    nodeSpan.innerText = postDate + " ";
+    nodeSpan.classList.add("post-dates");
+    node.appendChild(nodeSpan);
+    let nodeA = document.createElement("a");
+    nodeA.href = postPath;
+    nodeA.innerText = postName;
+    nodeA.classList.add("post-links");
+    node.appendChild(nodeA);
+    document.getElementById("monochrome-posts").appendChild(node);
+  }
+}
+
+// Fetch all the published monochrome posts.
+let checkMonochrome = document.getElementById("monochrome-posts");
+if (checkMonochrome !== null) {
+  fetch("/posts/monochrome/monochrome.json")
+    .then((response) => response.json())
+    .then((data) => appendMonochromePosts(data));
+}
+
+// MONOCHROME [ END ]
